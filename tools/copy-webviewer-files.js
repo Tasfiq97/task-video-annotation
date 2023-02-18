a2562beeba4e4d203aa94f7bb7d1505a4c1cb6f6
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-02-18 21:31:25 +0600

    deploy done

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -5,7 +5,6 @@ const copyFiles = async () => {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
     await fs.copy('./node_modules/@pdftron/webviewer/webviewer.min.js', './public/webviewer/lib/webviewer.min.js');
     await fs.copy('./node_modules/@pdftron/webviewer-video/src/lib/api/ServiceWorker/wv-video-injection.js', './public/wv-video-injection.js');
-    console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
   }
